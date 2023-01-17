395384d12f9d0c933a30827669f25fdf9dfa4b20
Kasper Nilssen <kasper.nilssen1@gmail.com>
2023-01-17 18:50:42 +0100

    create a logo component

---
+++ src/components/logo.tsx
@@ -0,0 +1,12 @@
+import { HStack, Text } from "@chakra-ui/react";
+import logo from "../../public/logo.svg";
+import Image from "next/image";
+
+export default function Logo() {
+  return (
+    <HStack spacing={1} sx={{"userSelect": "none"}}>
+      <Image color={"red"} height={56} width={56} alt={"logo"} src={logo} />
+      <Text fontWeight={600} letterSpacing={2} fontSize={"4xl"}>VERIFIRE</Text>
+    </HStack>
+  );
+}
