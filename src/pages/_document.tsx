e75b0e093985d16697ad39243fdaf13dedef34d4
nilssen98 <kasper.nilssen1@gmail.com>
2023-01-17 03:03:15 +0100

    remove unused import

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,6 +1,5 @@
 import { ColorModeScript } from "@chakra-ui/react";
 import { Html, Head, Main, NextScript } from "next/document";
-import theme from "../theme";
 
 export default function Document() {
   return (
