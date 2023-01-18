837a5c1acbee7baa3b64a4ecaad09708cfb773dd
Kasper Nilssen <kasper.nilssen1@gmail.com>
2023-01-18 14:39:33 +0100

    prettify

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -4,9 +4,11 @@ import Image from "next/image";
 
 export default function Logo() {
   return (
-    <HStack spacing={1} sx={{"userSelect": "none"}}>
+    <HStack spacing={1} sx={{ userSelect: "none" }}>
       <Image color={"red"} height={56} width={56} alt={"logo"} src={logo} />
-      <Text fontWeight={600} letterSpacing={3} fontSize={"4xl"}>CONFIGIFY</Text>
+      <Text fontWeight={600} letterSpacing={3} fontSize={"4xl"}>
+        CONFIGIFY
+      </Text>
     </HStack>
   );
 }
