4e8f558d6b46a1e92f38d8dadd82e6910e0bb772
Kasper Nilssen <kasper.nilssen1@gmail.com>
2023-01-18 15:42:33 +0100

    add polka dots to background

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,10 +1,19 @@
 import { Box } from "@chakra-ui/react";
-import { ReactElement } from "react";
+import type { ReactElement } from "react";
 
 export default function Layout({ children }: { children: ReactElement }) {
   return (
     <>
-      <Box minH={"100vh"}>{children}</Box>
+      <Box
+        minH={"100vh"}
+        sx={{
+          backgroundImage: `radial-gradient(#343a40 2px, transparent 2px), radial-gradient(#343a40 2px, transparent 2px)`,
+          backgroundSize: "120px 120px",
+          backgroundPosition: "0 0, 60px 60px",
+        }}
+      >
+        {children}
+      </Box>
     </>
   );
 }
