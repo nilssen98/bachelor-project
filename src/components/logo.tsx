ee78eafc7f9314ab24a553b3573bc9003c2f667b
Kasper Nilssen <kasper.nilssen1@gmail.com>
2023-01-18 15:42:15 +0100

    prettify, adjust spacing and font weight

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -4,9 +4,15 @@ import Image from "next/image";
 
 export default function Logo() {
   return (
-    <HStack spacing={1} sx={{ userSelect: "none" }}>
-      <Image color={"red"} height={56} width={56} alt={"logo"} src={logo} />
-      <Text fontWeight={600} letterSpacing={3} fontSize={"4xl"}>
+    <HStack spacing={4} sx={{ userSelect: "none" }}>
+      <Image
+        color={"red"}
+        height={56}
+        width={56}
+        alt={"logo"}
+        src={logo as string}
+      />
+      <Text letterSpacing={3} fontSize={"4xl"}>
         CONFIGIFY
       </Text>
     </HStack>
