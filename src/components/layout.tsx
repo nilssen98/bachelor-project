e283f1226a3fee03791c80d90983f415497ff216
Marko19907 <marko.stancic23@gmail.com>
2023-01-20 13:07:41 +0100

    Created the random background icon placement algo

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,9 +1,11 @@
 import { Box } from "@chakra-ui/react";
 import type { ReactElement } from "react";
+import BackgroundCollection from "./background-collection";
 
 export default function Layout({ children }: { children: ReactElement }) {
   return (
     <>
+      <BackgroundCollection count={20} imageWidth={50} imageHeight={50} minDistance={100}/>
       <Box
         minH={"100vh"}
         sx={{
