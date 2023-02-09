04fa8cc7ae239400150b29122b4cd6df121ae929
Kasper Nilssen <43757174+nilssen98@users.noreply.github.com>
2023-02-09 19:29:10 +0100

    Trpc template router (#3)
    * add template router
    
    * add template router with getAll, get and add endpoints
    
    * add delete endpoint to template router

---
+++ src/server/api/routers/template.ts
@@ -0,0 +1,51 @@
+import { z } from "zod";
+import { createTRPCRouter, protectedProcedure } from "../trpc";
+
+export const templateRouter = createTRPCRouter({
+  getAll: protectedProcedure.query(({ ctx }) => {
+    return ctx.prisma.template.findMany({
+      where: {
+        userId: ctx.session.user.id,
+      },
+    });
+  }),
+  get: protectedProcedure
+    .input(z.object({ id: z.string() }))
+    .query(({ ctx, input }) => {
+      return ctx.prisma.template.findFirstOrThrow({
+        where: {
+          id: input.id,
+          userId: ctx.session.user.id,
+        },
+      });
+    }),
+  add: protectedProcedure
+    .input(
+      z.object({
+        name: z.string(),
+        content: z.string(),
+      })
+    )
+    .mutation(({ ctx, input }) => {
+      return ctx.prisma.template.create({
+        data: {
+          ...input,
+          userId: ctx.session.user.id,
+        },
+      });
+    }),
+  delete: protectedProcedure
+    .input(
+      z.object({
+        id: z.string(),
+      })
+    )
+    .mutation(({ ctx, input }) => {
+      return ctx.prisma.template.deleteMany({
+        where: {
+          id: input.id,
+          userId: ctx.session.user.id,
+        },
+      });
+    }),
+});
