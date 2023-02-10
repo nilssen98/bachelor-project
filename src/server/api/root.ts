791dec48ff750e482e11f6f3ae5c4e2bde9a01cb
Kasper Nilssen <43757174+nilssen98@users.noreply.github.com>
2023-02-10 14:49:18 +0100

    add trpc configuration routes (#5)
    * add configurations to the user schema
    
    * add configuration router with CRUD operations
    
    * add configuration router to the app router
    
    * remove example schema
    
    * remove the example router from the app router
    
    * remove the example router

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,6 +1,6 @@
 import { createTRPCRouter } from "./trpc";
-import { exampleRouter } from "./routers/example";
 import { templateRouter } from "./routers/template";
+import { configurationRouter } from "./routers/configuration";
 
 /**
  * This is the primary router for your server.
@@ -8,8 +8,8 @@ import { templateRouter } from "./routers/template";
  * All routers added in /api/routers should be manually added here
  */
 export const appRouter = createTRPCRouter({
-  example: exampleRouter,
   template: templateRouter,
+  configuration: configurationRouter,
 });
 
 // export type definition of API
