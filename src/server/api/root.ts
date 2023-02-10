e391fadb252c521403ac41bd335f6dafa607fe28
Kasper Nilssen <kasper.nilssen1@gmail.com>
2023-02-10 14:46:59 +0100

    add configuration router to the app router

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,6 +1,7 @@
 import { createTRPCRouter } from "./trpc";
 import { exampleRouter } from "./routers/example";
 import { templateRouter } from "./routers/template";
+import { configurationRouter } from "./routers/configuration";
 
 /**
  * This is the primary router for your server.
@@ -10,6 +11,7 @@ import { templateRouter } from "./routers/template";
 export const appRouter = createTRPCRouter({
   example: exampleRouter,
   template: templateRouter,
+  configuration: configurationRouter,
 });
 
 // export type definition of API
