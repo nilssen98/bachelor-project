33c9b076715a0a42403dcaec524568245f58f8fd
Kasper Nilssen <kasper.nilssen1@gmail.com>
2023-02-10 18:25:12 +0100

    add fetching and displaying templates

--- src/pages/templates.tsx
+++ src/pages/templates.tsx
@@ -1,7 +1,18 @@
-import { Button, HStack, Input, Text, VStack } from "@chakra-ui/react";
+import {
+  Button,
+  Card,
+  Grid,
+  GridItem,
+  HStack,
+  Input,
+  Text,
+  VStack,
+} from "@chakra-ui/react";
 import { type NextPage } from "next";
+import { api } from "../utils/api";
 
 const TemplatesPage: NextPage = () => {
+  const { data: templates } = api.template.getAll.useQuery();
   return (
     <>
       <VStack alignItems={"flex-start"} spacing={4} width={"full"}>
@@ -10,6 +21,15 @@ const TemplatesPage: NextPage = () => {
           <Button>Add new</Button>
           <Input placeholder={"Search"} />
         </HStack>
+        <Grid w={"full"} templateColumns="repeat(5, 1fr)" gap={4}>
+          {templates?.map((template, idx) => (
+            <GridItem w={"100%"} key={idx}>
+              <Card p={4}>
+                <Text>{template.name}</Text>
+              </Card>
+            </GridItem>
+          ))}
+        </Grid>
       </VStack>
     </>
   );
