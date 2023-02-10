6de6ed2e347f30af4cc6c9b494edc67539a7ce33
Kasper Nilssen <kasper.nilssen1@gmail.com>
2023-02-10 18:13:52 +0100

    add middleware for auth redirections

---
+++ src/middleware.ts
@@ -0,0 +1,17 @@
+import { withAuth } from "next-auth/middleware";
+import { NextResponse } from "next/server";
+
+export default withAuth(function middleware(req) {
+  const token = req.nextauth.token;
+  if (!token) {
+    return NextResponse.redirect(new URL("/auth/signin", req.url));
+  }
+
+  if (req.nextUrl.pathname === "/") {
+    return NextResponse.redirect(new URL("/templates", req.url));
+  }
+
+  return NextResponse.next();
+});
+
+export const config = { matcher: ["/", "/templates/:path*", "/profile"] };
