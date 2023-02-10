598189162a16b60b4980d830cb2f352f3a375c4e
Kasper Nilssen <kasper.nilssen1@gmail.com>
2023-02-10 15:09:12 +0100

    improve the layout, center content in the middle

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,21 +1,30 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Flex } from "@chakra-ui/react";
 import type { ReactElement } from "react";
 import BackgroundCollection from "./background-collection";
 
 export default function Layout({ children }: { children: ReactElement }) {
   return (
     <>
-      <BackgroundCollection count={20} imageWidth={50} imageHeight={50} minDistance={100}/>
-      <Box
-        minH={"100vh"}
+      {/* <BackgroundCollection
+        count={20}
+        imageWidth={50}
+        imageHeight={50}
+        minDistance={100}
+      /> */}
+      <Flex
+        height={"100vh"}
+        width={"100vw"}
+        justifyContent={"center"}
         sx={{
           backgroundImage: `radial-gradient(#343a40 2px, transparent 2px), radial-gradient(#343a40 2px, transparent 2px)`,
           backgroundSize: "120px 120px",
           backgroundPosition: "0 0, 60px 60px",
         }}
       >
-        {children}
-      </Box>
+        <Box maxW={"5xl"} w={"100%"}>
+          {children}
+        </Box>
+      </Flex>
     </>
   );
 }
