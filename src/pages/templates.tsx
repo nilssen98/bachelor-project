caa289443e4d6bafb8aaa3383469ff391a6209a8
Daniel Bjørdal <danbjo2001@gmail.com>
2023-02-10 15:41:36 +0100

    changed width from 100% to 'full' and added typing

--- src/pages/templates.tsx
+++ src/pages/templates.tsx
@@ -1,15 +1,18 @@
 import { Button, HStack, Input, Text, VStack } from "@chakra-ui/react";
+import { type NextPage } from "next";
 
-export default function TemplatesPage() {
+const TemplatesPage: NextPage = () => {
   return (
     <>
-      <VStack alignItems={"flex-start"} spacing={4} width={"100%"}>
+      <VStack alignItems={"flex-start"} spacing={4} width={"full"}>
         <Text fontSize={"5xl"}>Templates</Text>
-        <HStack width={"100%"}>
+        <HStack width={"full"}>
           <Button>Add new</Button>
           <Input placeholder={"Search"} />
         </HStack>
       </VStack>
     </>
   );
-}
+};
+
+export default TemplatesPage;
