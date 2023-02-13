d33ae2f22b95e8ce1cfd7c73b91fc826e9b452de
Marko19907 <marko.stancic23@gmail.com>
2023-02-13 15:43:32 +0100

    Made the logo more customizable

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,18 +1,27 @@
 import { HStack, Text } from "@chakra-ui/react";
-import logo from "../../public/logo.svg";
+import type { StackProps, TypographyProps } from "@chakra-ui/react";
 import Image from "next/image";
+import logo from "../../public/logo.svg";
 
-export default function Logo() {
+type Props = {
+  logoHeight?: number;
+  fontSize?: TypographyProps["fontSize"];
+} & StackProps;
+
+export default function Logo(props: Props) {
   return (
-    <HStack spacing={4} sx={{ userSelect: "none" }}>
+    <HStack spacing={4} sx={{ userSelect: "none" }} {...props}>
       <Image
         color={"red"}
-        height={56}
-        width={56}
         alt={"logo"}
         src={logo as string}
+        draggable={false}
+        style={{
+          height: props.logoHeight || 42,
+          width: props.logoHeight || 42,
+        }}
       />
-      <Text letterSpacing={3} fontSize={"4xl"}>
+      <Text letterSpacing={3} fontSize={props.fontSize || "4xl"}>
         CONFIGIFY
       </Text>
     </HStack>
