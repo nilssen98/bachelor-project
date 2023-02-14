27ab7f9c296b4ac7009c88f28bed58e56ea56c0f
Marko19907 <marko.stancic23@gmail.com>
2023-02-14 13:02:32 +0100

    Created the initial version of the profile page

---
+++ src/pages/profile.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from "next";
+import { Text } from "@chakra-ui/react";
+
+const ProfilePage: NextPage = () => {
+  return (
+    <>
+      <Text>Profile page</Text>
+    </>
+  );
+};
+
+export default ProfilePage;
