9880d5df1cc1a265a0e8943ad682811a406d5ca4
Marko19907 <marko.stancic23@gmail.com>
2023-02-14 16:32:40 +0100

    The correct layout is now selected in the App

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,4 @@
+import type { AppProps } from "next/app";
 import { type AppType } from "next/app";
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
@@ -5,6 +6,13 @@ import { api } from "../utils/api";
 import { ChakraProvider, cookieStorageManager } from "@chakra-ui/react";
 import theme from "../theme";
 import Layout from "../components/layout";
+import React from "react";
+
+type ComponentWithPageLayout = AppProps & {
+  Component: AppProps["Component"] & {
+    PageLayout?: React.ComponentType;
+  };
+};
 
 const App: AppType<{ session: Session | null }> = ({
   Component,
@@ -14,7 +22,13 @@ const App: AppType<{ session: Session | null }> = ({
     <SessionProvider session={session}>
       <ChakraProvider colorModeManager={cookieStorageManager} theme={theme}>
         <Layout>
-          <Component {...pageProps} />
+          {Component.PageLayout ? (
+            <Component.PageLayout>
+              <Component {...pageProps} />
+            </Component.PageLayout>
+          ) : (
+            <Component {...pageProps} />
+          )}
         </Layout>
       </ChakraProvider>
     </SessionProvider>
