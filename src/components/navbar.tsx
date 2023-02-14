f6b761c6c6c62dd4f2aa95bed3eb272bc5477445
Marko19907 <marko.stancic23@gmail.com>
2023-02-14 13:45:03 +0100

    The navbar now has a max width

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -13,7 +13,11 @@ export default function Navbar(props: Props) {
         as={"nav"}
         h={`${props.height}px`}
         p={1}
+        maxW={"5xl"}
         justify={"space-between"}
+        sx={{
+          margin: "0 auto",
+        }}
       >
         <Logo fontSize={"1xl"} logoHeight={32} spacing={1.5} />
         <Link href={"/profile"}>
