333c7e4fafec016a5a9238e175bd9d8491c2eca1
Kasper Nilssen <kasper.nilssen1@gmail.com>
2023-02-16 13:17:28 +0100

    add correct typing to the App props

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,30 +6,28 @@ import { api } from "../utils/api";
 import { ChakraProvider, cookieStorageManager } from "@chakra-ui/react";
 import theme from "../theme";
 import Layout from "../components/layout";
+import type { ReactElement, ReactNode } from "react";
 import React from "react";
+import type { NextPage } from "next";
 
-type ComponentWithPageLayout = AppProps & {
-  Component: AppProps["Component"] & {
-    PageLayout?: React.ComponentType;
-  };
+export type NextPageWithLayout<P = object, IP = P> = NextPage<P, IP> & {
+  getLayout?: (page: ReactElement | ReactNode) => ReactNode;
 };
 
-const App: AppType<{ session: Session | null }> = ({
-  Component,
-  pageProps: { session, ...pageProps },
-}) => {
+interface Props {
+  Component: NextPageWithLayout;
+  pageProps: {
+    session: Session;
+  };
+}
+
+const App = ({ Component, pageProps: { session, ...pageProps } }: Props) => {
+  const getLayout = Component.getLayout ?? ((page) => page);
+
   return (
     <SessionProvider session={session}>
       <ChakraProvider colorModeManager={cookieStorageManager} theme={theme}>
-        <Layout>
-          {Component.PageLayout ? (
-            <Component.PageLayout>
-              <Component {...pageProps} />
-            </Component.PageLayout>
-          ) : (
-            <Component {...pageProps} />
-          )}
-        </Layout>
+        <Layout>{getLayout(<Component {...pageProps} />)}</Layout>
       </ChakraProvider>
     </SessionProvider>
   );
