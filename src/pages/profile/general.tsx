d7f60a7f638ac98bfe90b5023ec0bf5f7d3f248b
Marko19907 <marko.stancic23@gmail.com>
2023-02-20 17:11:27 +0100

    The email is now displayed on the profile page for testing

--- src/pages/profile/general.tsx
+++ src/pages/profile/general.tsx
@@ -1,15 +1,20 @@
 import type { ReactElement } from "react";
 import { Button, Text } from "@chakra-ui/react";
-import { signOut } from "next-auth/react";
+import { signOut, useSession } from "next-auth/react";
 import { MdLogout } from "react-icons/md";
 import type { NextPageWithLayout } from "../_app";
 import ProfileSidebarLayout from "../../components/profileSidebarLayout";
 
 const GeneralPage: NextPageWithLayout = () => {
+  const session = useSession();
+
   return (
     <>
       <Text fontSize={"4xl"} my={4}>
         General page!
+      </Text>
+      <Text fontSize={"xl"}>
+        {`Email: ${session.data?.user?.email || "No email"}`}
       </Text>
       <Button onClick={() => void signOut()} leftIcon={<MdLogout />}>
         Sign out
