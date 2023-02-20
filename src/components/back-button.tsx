dd88cc768f1ec9f6523f854f5a797a056df9a4c3
Kasper Nilssen <kasper.nilssen1@gmail.com>
2023-02-20 14:36:51 +0100

    add an optional href prop

--- src/components/back-button.tsx
+++ src/components/back-button.tsx
@@ -1,15 +1,31 @@
+import type { ButtonProps } from "@chakra-ui/react";
 import { Button, Icon, Text } from "@chakra-ui/react";
+import { omit } from "lodash-es";
 import { useRouter } from "next/router";
 import { MdKeyboardArrowLeft } from "react-icons/md";
 
-export default function BackButton() {
+type Props = {
+  href?: string;
+} & ButtonProps;
+
+export default function BackButton(props: Props) {
   const router = useRouter();
+
+  const handleClick = async () => {
+    if (props.href) {
+      await router.push(props.href);
+    } else {
+      router.back();
+    }
+  };
+
   return (
     <Button
       px={0}
       leftIcon={<Icon as={MdKeyboardArrowLeft} />}
       variant={"text"}
-      onClick={() => router.back()}
+      onClick={() => void handleClick()}
+      {...omit(props, "href")}
     >
       <Text alignSelf="center">Back</Text>
     </Button>
