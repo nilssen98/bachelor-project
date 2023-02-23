5791f10c7874e33ae71f95dc83f5ec8ab6de3a49
Marko19907 <marko.stancic23@gmail.com>
2023-02-23 13:22:02 +0100

    Displaying the session id on the connections page

--- src/pages/profile/connections.tsx
+++ src/pages/profile/connections.tsx
@@ -1,14 +1,21 @@
 import type { ReactElement } from "react";
 import { Text } from "@chakra-ui/react";
+import { useSession } from "next-auth/react";
+import { api } from "../../utils/api";
 import type { NextPageWithLayout } from "../_app";
 import ProfileSidebarLayout from "../../components/profileSidebarLayout";
 
 const ConnectionPage: NextPageWithLayout = () => {
+  const session = useSession();
+
+  const { data: me, isLoading: isLoadingMe } = api.me.get.useQuery();
+
   return (
     <>
       <Text fontSize={"4xl"} my={4}>
         Connections page!
       </Text>
+      {session.data?.user?.id}
     </>
   );
 };
