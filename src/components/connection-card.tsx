e41a66518c66290fcc399cb61610e7ec32828932
Marko19907 <marko.stancic23@gmail.com>
2023-02-24 15:56:19 +0100

    Added a Disconnect button to the ConnectionCard

--- src/components/connection-card.tsx
+++ src/components/connection-card.tsx
@@ -1,25 +1,62 @@
-import { Card, CardBody, HStack, Text } from "@chakra-ui/react";
-import { getLogo } from "../pages/auth/signin";
+import {
+  Card,
+  CardBody,
+  HStack,
+  Icon,
+  IconButton,
+  Text,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
+} from "@chakra-ui/react";
+import { FaGoogle } from "react-icons/fa";
+import { IoMailOpenOutline } from "react-icons/io5";
+import { BiDotsVerticalRounded } from "react-icons/bi";
 
 interface Props {
   provider: string;
 }
+
+function getLogo(provider: string): JSX.Element {
+  const fontSize = "md";
+  switch (provider.toLowerCase()) {
+    case "google":
+      return <Icon as={FaGoogle} fontSize={fontSize} />;
+    case "email":
+      return <Icon as={IoMailOpenOutline} fontSize={fontSize} />;
+    default:
+      return <Text>No logo</Text>;
+  }
+}
+
 export default function ConnectionCard(props: Props) {
   return (
     <>
-      <Card width={"full"} height={16} variant={"outline"}>
+      <Card width={"full"} variant={"outline"}>
         <CardBody>
-          <HStack alignItems={"center"} spacing={2}>
-            {getLogo(props.provider)}
-            <Text
-              fontSize={"sm"}
-              fontWeight={"bold"}
-              sx={{
-                textTransform: "capitalize",
-              }}
-            >
-              {props.provider}
-            </Text>
+          <HStack justifyContent={"space-between"} alignItems={"center"}>
+            <HStack alignItems={"center"} spacing={2}>
+              {getLogo(props.provider)}
+              <Text
+                fontSize={"sm"}
+                sx={{
+                  textTransform: "capitalize",
+                }}
+              >
+                {props.provider}
+              </Text>
+            </HStack>
+            <Menu>
+              <MenuButton
+                background={"none"}
+                as={IconButton}
+                icon={<Icon as={BiDotsVerticalRounded} />}
+              />
+              <MenuList>
+                <MenuItem color={"red.500"}>Disconnect</MenuItem>
+              </MenuList>
+            </Menu>
           </HStack>
         </CardBody>
       </Card>
