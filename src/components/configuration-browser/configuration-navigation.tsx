24be2cae72b487ab6de94927005078242b747ff7
Kasper Nilssen <kasper.nilssen1@gmail.com>
2023-02-27 19:19:39 +0100

    add an home icon for root, add tags to the navigation names

--- src/components/configuration-browser/configuration-navigation.tsx
+++ src/components/configuration-browser/configuration-navigation.tsx
@@ -3,21 +3,35 @@ import {
   Breadcrumb,
   BreadcrumbItem,
   BreadcrumbLink,
+  Center,
+  Icon,
+  Stack,
+  Tag,
+  Text,
 } from "@chakra-ui/react";
+import { MdHome } from "react-icons/md";
 import { useConfiguration } from "./configuration-provider";
 
 export default function ConfigurationNavigation() {
   const { path, navigate } = useConfiguration();
   return (
     <>
-      <Box p={4}>
+      <Box p={4} overflowX={"auto"}>
         <Breadcrumb>
           <BreadcrumbItem onClick={() => navigate(0)}>
-            <BreadcrumbLink>root</BreadcrumbLink>
+            <BreadcrumbLink alignItems={"center"}>
+              <Center>
+                <Icon fontSize={"2xl"} as={MdHome} />
+              </Center>
+            </BreadcrumbLink>
           </BreadcrumbItem>
           {path.map((path, idx) => (
             <BreadcrumbItem onClick={() => navigate(idx + 1)} key={idx}>
-              <BreadcrumbLink>{path}</BreadcrumbLink>
+              <BreadcrumbLink>
+                <Tag>
+                  <Text fontSize={"md"}>{path}</Text>
+                </Tag>
+              </BreadcrumbLink>
             </BreadcrumbItem>
           ))}
         </Breadcrumb>
