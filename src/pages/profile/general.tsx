f3f2abaf08f8938d6477b54e20b227f39c7b82ae
Marko19907 <marko.stancic23@gmail.com>
2023-02-27 16:46:41 +0100

    Improved the general page

--- src/pages/profile/general.tsx
+++ src/pages/profile/general.tsx
@@ -1,9 +1,10 @@
 import type { ReactElement } from "react";
-import { Button, Text } from "@chakra-ui/react";
+import { Button, HStack, Text, VStack } from "@chakra-ui/react";
 import { signOut, useSession } from "next-auth/react";
 import { MdLogout } from "react-icons/md";
 import type { NextPageWithLayout } from "../_app";
 import ProfileSidebarLayout from "../../components/profile-sidebar-layout";
+import UserAvatar from "../../components/user-avatar";
 
 const GeneralPage: NextPageWithLayout = () => {
   const session = useSession();
@@ -11,19 +12,34 @@ const GeneralPage: NextPageWithLayout = () => {
   return (
     <>
       <Text fontSize={"4xl"} my={4}>
-        General page!
+        General
       </Text>
-      <Text fontSize={"xl"}>
-        {`Email: ${session.data?.user?.email || "No email"}`}
-      </Text>
-      <Button
-        color={"red.300"}
-        _hover={{ color: "red.400", bg: "red.900" }}
-        onClick={() => void signOut()}
-        leftIcon={<MdLogout />}
-      >
-        Sign out
-      </Button>
+      <HStack justifyContent={"space-between"}>
+        <HStack spacing={4}>
+          <UserAvatar
+            sx={{
+              height: "100px",
+              width: "100px",
+            }}
+          />
+          <VStack alignItems={"flex-start"}>
+            <Text fontSize={"3xl"}>
+              {session.data?.user?.name || "No name"}
+            </Text>
+            <Text fontSize={"1xl"}>
+              {session.data?.user?.email || "No email"}
+            </Text>
+          </VStack>
+        </HStack>
+        <Button
+          color={"red.300"}
+          _hover={{ color: "red.400", bg: "red.900" }}
+          onClick={() => void signOut()}
+          leftIcon={<MdLogout />}
+        >
+          Sign out
+        </Button>
+      </HStack>
     </>
   );
 };
