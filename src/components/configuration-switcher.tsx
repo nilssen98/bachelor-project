037b1ea1da07626e066c216db7eff02e756a6c27
iTormentor <toranders98@gmail.com>
2023-03-06 15:11:32 +0100

    Added hook for window resize, and continued refactor of chip logic

--- src/components/configuration-switcher.tsx
+++ src/components/configuration-switcher.tsx
@@ -2,7 +2,9 @@ import { Button, Grid, GridItem } from "@chakra-ui/react";
 import Chip from "./chip";
 import { api } from "../utils/api";
 import { useRouter } from "next/router";
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
+import { initial } from "lodash-es";
+import { useWindowResize } from "../hooks/useWindowResize";
 
 interface Props {
   templateId?: string;
@@ -20,10 +22,20 @@ export default function ConfigurationSwitcher(props: Props) {
   );
   const [expanded, setExpanded] = useState<boolean>(false);
 
-  const nonExpandedDisplay =
-    configurations != undefined && configurations.length > 8 ? 7 : 8;
+  const [width] = useWindowResize();
 
-  const [displayCount, setDisplayCount] = useState<number>(nonExpandedDisplay);
+  const [displayCount, setDisplayCount] = useState<number>(8);
+  const [elementsRow, setElementsRow] = useState<number>(8);
+
+  function findElementsPerRow() {
+    const size = Math.floor((width - 32) / 130);
+    setElementsRow(size > 8 ? 8 : size);
+    console.log("Elements per row: ", elementsRow);
+  }
+
+  useEffect(() => {
+    findElementsPerRow();
+  }, [width]);
 
   const handleChipClick = async (configurationId: string) => {
     await router.push(`/configurations/${configurationId}`);
@@ -31,16 +43,23 @@ export default function ConfigurationSwitcher(props: Props) {
 
   const handleExpandButton = () => {
     setExpanded(!expanded);
-    if (!expanded) {
-      setDisplayCount(configurations != undefined ? configurations.length : 8);
-    } else {
-      setDisplayCount(nonExpandedDisplay);
-    }
+    // if (!expanded) {
+    //  setDisplayCount(configurations != undefined ? configurations.length : 8);
+    //} else {
+    //  setDisplayCount(nonExpandedDisplay);
+    //}
+    someFunctionToCalculateChips();
   };
 
-  function findChipsPerRow() {
-    const size = Math.floor(window.innerWidth / 120);
-    return size > 8 ? 8 : size;
+  function someFunctionToCalculateChips() {
+    if (
+      !expanded ||
+      (configurations != undefined && configurations.length == elementsRow)
+    ) {
+      setDisplayCount(elementsRow);
+    } else {
+      setDisplayCount(elementsRow - 1);
+    }
   }
 
   function chipExpandButton() {
@@ -59,7 +78,7 @@ export default function ConfigurationSwitcher(props: Props) {
         gap={2}
         w={"full"}
         paddingY={2}
-        templateColumns={`repeat(${findChipsPerRow()}, 1fr)`}
+        templateColumns={`repeat(${elementsRow}, 1fr)`}
       >
         {configurations?.slice(0, displayCount).map((configuration, idx) => (
           <GridItem w={"100%"} key={idx}>
---
+++ src/hooks/useWindowResize.ts
@@ -0,0 +1,26 @@
+import { useEffect, useState } from "react";
+
+type TWindowSize = [number, number];
+
+type THook = TWindowSize;
+
+export const useWindowResize = (): THook => {
+  const initSize: TWindowSize = [window.innerWidth, window.innerHeight];
+  const [windowSize, setWindowSize] = useState<TWindowSize>(initSize);
+
+  useEffect(() => {
+    const handleResize = (): void => {
+      setWindowSize([window.innerWidth, window.innerHeight]);
+    };
+
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  return windowSize;
+};
+
+// Example of usage
+// const [width, height] = useWindowResize();
