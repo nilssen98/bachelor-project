b0b79e2d98496a51bd9599cc8abfe2135db7c544
Daniel Bjørdal <danbjo2001@gmail.com>
2023-03-21 17:12:24 +0100

    email input field on sign in will now be red if format is invalid

--- src/components/email-input.tsx
+++ src/components/email-input.tsx
@@ -2,13 +2,22 @@ import { Icon, Input, InputGroup, InputLeftElement } from "@chakra-ui/react";
 import type { InputProps } from "@chakra-ui/react";
 import { MdOutlineEmail } from "react-icons/md";
 
-type Props = InputProps;
+type Props = {
+  verify?: boolean;
+} & InputProps;
 
 export default function EmailInput(props: Props) {
+  function isValid() {
+    if (props.verify) {
+      return /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(props.value as string);
+    }
+    return true;
+  }
+
   return (
     <>
       <InputGroup>
-        <Input {...props} placeholder={"Email"} />
+        <Input {...props} on isInvalid={!isValid()} placeholder={"Email"} />
         <InputLeftElement>
           <Icon as={MdOutlineEmail} />
         </InputLeftElement>
