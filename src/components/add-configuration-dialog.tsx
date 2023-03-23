2c3225e56a6e64aea92b8022a0b1f2544e4f1268
Marko19907 <marko.stancic23@gmail.com>
2023-03-23 17:30:28 +0100

    Added a general layout and navigation for the dialog

--- src/components/add-configuration-dialog.tsx
+++ src/components/add-configuration-dialog.tsx
@@ -1,4 +1,5 @@
-import { Button } from "@chakra-ui/react";
+import { useState } from "react";
+import { Button, HStack, Text } from "@chakra-ui/react";
 import {
   Modal,
   ModalBody,
@@ -10,21 +11,92 @@ import {
 } from "@chakra-ui/modal";
 import type { ModalProps } from "@chakra-ui/modal";
 
+enum Steps {
+  ChooseAction = 0,
+  CreateNew = 1,
+  CloneExisting = 2,
+  UploadFile = 3,
+}
+
 type Props = Omit<ModalProps, "children">;
 
 export default function AddConfigurationDialog(props: Props) {
+  const [step, setStep] = useState<Steps>(Steps.ChooseAction);
+
+  function getTitle() {
+    switch (step) {
+      case Steps.ChooseAction:
+        return "Add Configuration";
+      case Steps.CreateNew:
+        return "Create New Configuration";
+      case Steps.CloneExisting:
+        return "Clone Existing Configuration";
+      case Steps.UploadFile:
+        return "Upload Configuration";
+    }
+  }
+
+  function getBody() {
+    switch (step) {
+      case Steps.ChooseAction:
+        return (
+          <>
+            <HStack>
+              <Button onClick={() => setStep(Steps.CreateNew)}>
+                Create new
+              </Button>
+              <Button onClick={() => setStep(Steps.CloneExisting)}>
+                Clone existing
+              </Button>
+              <Button onClick={() => setStep(Steps.UploadFile)}>
+                Upload file
+              </Button>
+            </HStack>
+          </>
+        );
+      case Steps.CreateNew:
+        return (
+          <>
+            <Text>Create new</Text>
+          </>
+        );
+      case Steps.CloneExisting:
+        return (
+          <>
+            <Text>Clone existing</Text>
+          </>
+        );
+      case Steps.UploadFile:
+        return (
+          <>
+            <Text>Upload file</Text>
+          </>
+        );
+    }
+  }
+
   return (
     <>
-      <Modal {...props}>
+      <Modal isCentered {...props}>
         <ModalOverlay />
         <ModalContent>
-          <ModalHeader>Create new configuration</ModalHeader>
+          <ModalHeader>{getTitle()}</ModalHeader>
           <ModalCloseButton onClick={props.onClose} />
-          <ModalBody>Dialog body</ModalBody>
+          <ModalBody>{getBody()}</ModalBody>
           <ModalFooter>
-            <Button colorScheme={"blue"} onClick={props.onClose}>
-              Close
-            </Button>
+            <HStack spacing={2}>
+              {step !== Steps.ChooseAction && (
+                <Button
+                  variant={"ghost"}
+                  onClick={() => setStep(Steps.ChooseAction)}
+                >
+                  Back
+                </Button>
+              )}
+              <Button colorScheme={"blue"} onClick={props.onClose}>
+                Close
+              </Button>
+            </HStack>
           </ModalFooter>
         </ModalContent>
       </Modal>
