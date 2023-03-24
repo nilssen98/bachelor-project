dc729076fce93d57448342dd5e1a77f361deb585
Marko19907 <marko.stancic23@gmail.com>
2023-03-24 11:28:11 +0100

    Completed the layout of the first dialog step

--- src/components/add-configuration-dialog.tsx
+++ src/components/add-configuration-dialog.tsx
@@ -1,5 +1,12 @@
 import { useState } from "react";
-import { Button, HStack, Text } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  HStack,
+  IconButton,
+  Text,
+  VStack,
+} from "@chakra-ui/react";
 import {
   Modal,
   ModalBody,
@@ -10,6 +17,9 @@ import {
   ModalOverlay,
 } from "@chakra-ui/modal";
 import type { ModalProps } from "@chakra-ui/modal";
+import { AiOutlineCopy, AiOutlineFileAdd } from "react-icons/ai";
+import { BsFiletypeJson } from "react-icons/bs";
+import { IoMdCopy } from "react-icons/io";
 
 enum Steps {
   ChooseAction = 0,
@@ -36,21 +46,70 @@ export default function AddConfigurationDialog(props: Props) {
     }
   }
 
+  function getCardButton(step: Steps) {
+    function getIcon() {
+      switch (step) {
+        case Steps.CreateNew:
+          return AiOutlineFileAdd;
+        case Steps.CloneExisting:
+          // return IoMdCopy;
+          return AiOutlineCopy;
+        case Steps.UploadFile:
+          return BsFiletypeJson;
+      }
+    }
+
+    function getText() {
+      switch (step) {
+        case Steps.CreateNew:
+          // return "New configuration from scratch";
+          return "Create a new configuration from scratch";
+        case Steps.CloneExisting:
+          // return "Clone an already existing configuration";
+          return "Create a copy of an already existing configuration";
+        case Steps.UploadFile:
+          return "Upload a configuration from a JSON file";
+      }
+    }
+
+    return (
+      <VStack
+        key={step}
+        flex={1}
+        height={180}
+        p={2}
+        as={"button"}
+        bg={"whiteAlpha.200"}
+        borderRadius={8}
+        onClick={() => setStep(step)}
+      >
+        <IconButton
+          flex={2}
+          as={getIcon()}
+          bg={"transparent"}
+          aria-label={"Create new configuration"}
+          _hover={{ bg: "transparent" }}
+        />
+        <Text>{getText()}</Text>
+      </VStack>
+    );
+  }
+
   function getBody() {
     switch (step) {
       case Steps.ChooseAction:
         return (
           <>
             <HStack>
-              <Button onClick={() => setStep(Steps.CreateNew)}>
-                Create new
-              </Button>
-              <Button onClick={() => setStep(Steps.CloneExisting)}>
-                Clone existing
-              </Button>
-              <Button onClick={() => setStep(Steps.UploadFile)}>
-                Upload file
-              </Button>
+              {Object.values(Steps).reduce((acc: JSX.Element[], key) => {
+                if (
+                  typeof Steps[key as keyof typeof Steps] === "number" &&
+                  Steps[key as keyof typeof Steps] !== Steps.ChooseAction
+                ) {
+                  acc.push(getCardButton(Steps[key as keyof typeof Steps]));
+                }
+                return acc;
+              }, [])}
             </HStack>
           </>
         );
@@ -77,7 +136,7 @@ export default function AddConfigurationDialog(props: Props) {
 
   return (
     <>
-      <Modal isCentered {...props}>
+      <Modal isCentered {...props} size={"xl"}>
         <ModalOverlay />
         <ModalContent>
           <ModalHeader>{getTitle()}</ModalHeader>
