e3cb0e1087308a0c3ea205c068d3fc8e0bb0f6c4
nilssen98 <kasper.nilssen1@gmail.com>
2023-04-16 22:10:02 +0200

    adjust colors, add tree view

--- src/components/configuration-browser/index.tsx
+++ src/components/configuration-browser/index.tsx
@@ -8,11 +8,14 @@ import BrowserNavigation from "./browser-navigation";
 import BrowserSideNavigation from "./browser-side-navigation";
 import BrowserStatusBar from "./browser-status-bar";
 import BrowserToolbar from "./browser-toolbar";
+import BrowserTreeView from "./browser-tree-view";
 import { useBrowserContent } from "./hooks/useBrowserContent";
+import { useBrowserController } from "./hooks/useBrowserController";
 
 export default function ConfigurationBrowser(props: StackProps) {
   const [searchValue, setSearchValue] = useState<string>("");
   const { isValidPath, configurations } = useBrowserContent();
+  const { showTreeView } = useBrowserController();
 
   const handleSave = () => {
     return;
@@ -30,26 +33,35 @@ export default function ConfigurationBrowser(props: StackProps) {
     <>
       <Stack flex={1} divider={<StackDivider />} spacing={0} {...props}>
         <HStack spacing={0} divider={<StackDivider />} flex={1}>
-          <Stack h={"full"} flex={0.2}>
+          <Stack spacing={0} h={"full"} w={300}>
             {configurations && configurations.length > 0 && (
-              <BrowserSideNavigation bg={"whiteAlpha.200"} />
+              <BrowserSideNavigation bg={"whiteAlpha.100"} />
             )}
           </Stack>
           <VStack spacing={0} divider={<StackDivider />} h={"full"} flex={1}>
             <BrowserToolbar
-              bg={"whiteAlpha.50"}
+              bg={"whiteAlpha.100"}
               onSave={handleSave}
               searchValue={searchValue}
               onSearchValueChanged={(newValue) => setSearchValue(newValue)}
               onClickErrors={handleClickErrors}
             />
-            <BrowserNavigation bg={"whiteAlpha.50"} />
-            <Stack overflow={"hidden"} bg={"whiteAlpha.50"} flex={1} w={"full"}>
+            {!showTreeView && <BrowserNavigation bg={"whiteAlpha.50"} />}
+            <Stack
+              spacing={0}
+              bg={"whiteAlpha.50"}
+              flex={1}
+              w={"full"}
+              position={"relative"}
+              overflowY={"auto"}
+              overflowX={"hidden"}
+            >
               <BrowserContent />
+              {showTreeView && <BrowserTreeView position={"absolute"} />}
             </Stack>
           </VStack>
         </HStack>
-        <BrowserStatusBar bg={"whiteAlpha.200"} />
+        <BrowserStatusBar bg={"whiteAlpha.100"} />
       </Stack>
     </>
   );
