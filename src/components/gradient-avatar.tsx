5a42f7e1251977cc463ed47cb7949720e0886598
nilssen98 <kasper.nilssen1@gmail.com>
2023-04-16 19:06:06 +0200

    spread avatar props properly

--- src/components/gradient-avatar.tsx
+++ src/components/gradient-avatar.tsx
@@ -32,7 +32,6 @@ export default function GradientAvatar(props: Props) {
 
   return (
     <Avatar
-      {...avatarProps}
       w={10}
       h={10}
       background={`linear-gradient(135deg, ${colors[0]!} 10%, ${colors[1]!} 90%)`}
@@ -41,6 +40,7 @@ export default function GradientAvatar(props: Props) {
         cursor: `${props.clickable ? "pointer" : "default"}`,
         ...avatarProps.sx,
       }}
+      {...avatarProps}
     />
   );
 }
