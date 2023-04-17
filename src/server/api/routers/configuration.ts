538f84560195991a93cb982586700d7f14974306
iTormentor <toranders98@gmail.com>
2023-04-17 13:09:48 +0200

    Added EditDialog to configurations as well, and fixed a bug in update for configs

--- src/server/api/routers/configuration.ts
+++ src/server/api/routers/configuration.ts
@@ -147,10 +147,10 @@ export const configurationRouter = createTRPCRouter({
         const jsonString = JSON.stringify(config.content, null, 2);
 
         // Create a Buffer from the JSON string
-        const buffer = Buffer.from(jsonString, 'utf-8');
+        const buffer = Buffer.from(jsonString, "utf-8");
 
         // Convert the buffer to a base64 string
-        const base64 = buffer.toString('base64');
+        const base64 = buffer.toString("base64");
 
         // Return the Blob and configuration name as a downloadable JSON file
         return {
@@ -162,30 +162,29 @@ export const configurationRouter = createTRPCRouter({
       }
     }),
   update: protectedProcedure
-      .input(
-          z.object({
-            id: z.string(),
-            name: z.string().optional(),
-            content: z.string().optional(),
-          })
-      )
-      .mutation(({ ctx, input }) => {
-        let parsedContent: Prisma.JsonValue | undefined = undefined;
+    .input(
+      z.object({
+        id: z.string(),
+        name: z.string().optional(),
+        content: z.string().optional(),
+      })
+    )
+    .mutation(({ ctx, input }) => {
+      let parsedContent: Prisma.JsonValue | undefined = undefined;
 
-        if (input.content) {
-          parsedContent = JSON.parse(input.content) as Prisma.JsonValue;
-        }
+      if (input.content) {
+        parsedContent = JSON.parse(input.content) as Prisma.JsonValue;
+      }
 
-        return ctx.prisma.template.updateMany({
-
-          where: {
-            id: input.id,
-            userId: ctx.session.user.id,
-          },
-          data: {
-            ...omit(input, "id", "content"),
-            ...(parsedContent ? { content: parsedContent } : {}),
-          }
-        });
-      }),
+      return ctx.prisma.configuration.updateMany({
+        where: {
+          id: input.id,
+          userId: ctx.session.user.id,
+        },
+        data: {
+          ...omit(input, "id", "content"),
+          ...(parsedContent ? { content: parsedContent } : {}),
+        },
+      });
+    }),
 });
