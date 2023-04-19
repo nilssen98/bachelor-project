aa059dfc10203f7235fbd3b5dd3ba4f8d0cf85ad
Kasper Nilssen <kasper.nilssen1@gmail.com>
2023-04-19 16:34:18 +0200

    add schema toggle

--- src/components/configuration-browser/browser-toolbar.tsx
+++ src/components/configuration-browser/browser-toolbar.tsx
@@ -24,7 +24,22 @@ type Props = {
 
 export default function BrowserToolbar(props: Props) {
   const { errors } = useBrowserContent();
-  const { toggleTreeView, showTreeView } = useBrowserController();
+  const { toggleTreeView, showTreeView, showSchema, toggleSchema } =
+    useBrowserController();
+
+  const handleShowTreeView = () => {
+    if (showSchema) {
+      toggleSchema();
+    }
+    toggleTreeView();
+  };
+
+  const handleShowSchema = () => {
+    if (showTreeView) {
+      toggleTreeView();
+    }
+    toggleSchema();
+  };
 
   const stackProps = omit(props, [
     "onSave",
@@ -41,7 +56,7 @@ export default function BrowserToolbar(props: Props) {
         spacing={0}
         {...stackProps}
       >
-        <Button
+        {/* <Button
           leftIcon={<Icon as={MdSave} />}
           onClick={props.onSave}
           fontWeight={400}
@@ -50,11 +65,11 @@ export default function BrowserToolbar(props: Props) {
           px={6}
         >
           Save
-        </Button>
+        </Button> */}
         <Button
           leftIcon={
             errors && errors.length > 0 ? (
-              <Icon as={MdErrorOutline} color={"red.500"} />
+              <Icon as={MdErrorOutline} color={"red.600"} />
             ) : undefined
           }
           onClick={props.onClickErrors}
@@ -70,15 +85,28 @@ export default function BrowserToolbar(props: Props) {
         </Button>
         <Button
           leftIcon={<Icon as={showTreeView ? IoMdEye : IoMdEyeOff} />}
-          onClick={toggleTreeView}
+          onClick={handleShowTreeView}
           fontWeight={400}
+          bg={showTreeView ? "whiteAlpha.200" : "default"}
           variant={"ghost"}
           borderRadius={0}
           px={6}
         >
           Tree view
         </Button>
-        <InputGroup>
+        <Button
+          leftIcon={<Icon as={showSchema ? IoMdEye : IoMdEyeOff} />}
+          onClick={handleShowSchema}
+          fontWeight={400}
+          bg={showSchema ? "whiteAlpha.200" : "default"}
+          variant={"ghost"}
+          borderRadius={0}
+          px={6}
+        >
+          Schema
+        </Button>
+        <Text></Text>
+        {/* <InputGroup>
           <InputLeftElement
             pointerEvents={"none"}
             // eslint-disable-next-line react/no-children-prop
@@ -90,7 +118,7 @@ export default function BrowserToolbar(props: Props) {
             variant={"unstyled"}
             placeholder={"Search"}
           />
-        </InputGroup>
+        </InputGroup> */}
       </Stack>
     </>
   );
