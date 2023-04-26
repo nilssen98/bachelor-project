66bc5e22100c464682e2ceebcb642ad2be7a9669
Kasper Nilssen <kasper.nilssen1@gmail.com>
2023-04-27 00:29:18 +0200

    Add edit and add handlers that update the configurations

--- src/components/configuration-browser/browser-tree-view.tsx
+++ src/components/configuration-browser/browser-tree-view.tsx
@@ -7,18 +7,36 @@ import { useBrowserController } from "./hooks/useBrowserController";
 
 const DynamicReactJson = dynamic(import("react-json-view"), { ssr: false });
 
-export default function BrowserTreeView(props: StackProps) {
-  const { configuration, template } = useBrowserContent();
+type Props = StackProps & {
+  onUpdate?: (newContent: object) => void;
+};
+
+export default function BrowserTreeView(props: Props) {
+  const { configuration, template, updateConfigurationContent } =
+    useBrowserContent();
   const { showTreeView, showSchema } = useBrowserController();
 
   const source = useMemo(() => {
+    console.log("updating source...");
     if (showTreeView) {
+      console.log("configuration chosen");
       return configuration.content;
     } else if (showSchema) {
+      console.log("schema chosen");
       return template.content;
     }
     return {};
   }, [configuration, template, showTreeView, showSchema]);
+
+  const handleEdit = ({ updated_src }: { updated_src: object }) => {
+    updateConfigurationContent(updated_src);
+  };
+
+  const handleAdd = ({ updated_src }: { updated_src: object }) => {
+    if (props.onUpdate) {
+      updateConfigurationContent(updated_src);
+    }
+  };
 
   return (
     <Stack width={"full"} height={"full"} {...props}>
@@ -27,6 +45,8 @@ export default function BrowserTreeView(props: StackProps) {
         enableClipboard={false}
         displayDataTypes={false}
         displayObjectSize={false}
+        onEdit={showSchema ? false : handleEdit}
+        onAdd={showSchema ? false : handleAdd}
         // name={configuration.name}
         style={{ padding: 16, backgroundColor: "#0A0A0A", height: "100%" }}
         src={source as object}
